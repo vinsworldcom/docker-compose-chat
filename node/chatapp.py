a05a8438019808e93eaa64437b574382204a9d7b
VinsWorldcom <vin@vinsworld.com>
2022-05-03 10:42:07 -0400

    better chatapp flow

--- node/chatapp.py
+++ node/chatapp.py
@@ -1,22 +1,38 @@
 #!/usr/local/bin/python3
 
 import os
+import sys
 import socket
 import threading
 
 PORT = 2222
 
+# Socket
 s = socket.socket(socket.AF_INET , socket.SOCK_DGRAM )
 s.bind(("0.0.0.0", PORT))
 
+# Local
+name = socket.gethostname()
+
+# Remote
+ip = None
+if len(sys.argv) >= 2:
+    ip = sys.argv[1]
+else:
+    ip = input("Connect to: ")
+    print("")
+try:
+    socket.getaddrinfo(ip, None, socket.AF_UNSPEC)
+except socket.gaierror as e:
+    print(e, file=sys.stderr)
+    exit(1)
+
+# Start
 print("====>  UDP CHAT APP  <=====")
 print("===========================")
-name = socket.gethostname()
 print("\nType 'quit' to exit.")
-ip = input("Connect to: ")
-if ip == "quit":
-    os._exit(0)
 
+# Subs
 def send():
     while True:
         message = input(">> ")
